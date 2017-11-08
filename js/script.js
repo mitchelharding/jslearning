d4b0c5e77e552bc024cfad9a2dff115ebbe4af55
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-07 17:20:41 -0700

    finalised script but need to find a way to mitigate script.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,99 @@
 $(document).ready(function(){
-  $("#java").click(function(){
-    $("#java1").show();
+
+  $(".java").click(function(){
+    $("p").hide();
+    $("#java1").toggle();
   });
+
+  $(".operator").click(function(){
+    $("p").hide();
+    $("#operators1").toggle();
+  });
+
+  $(".variables").click(function(){
+    $("p").hide();
+    $("#variables1").toggle();
+  });
+
+  $(".vnc").click(function(){
+    $("p").hide();
+    $("#vnc1").toggle();
+  });
+
+  $(".functions").click(function(){
+    $("p").hide();
+    $("#functions1").toggle();
+  });
+
+  $(".methods").click(function(){
+    $("p").hide();
+    $("#methods1").toggle();
+  });
+
+  $(".arguments").click(function(){
+    $("p").hide();
+    $("arguments1").toggle();
+  });
+
+  $(".parameters").click(function(){
+    $("p").hide();
+    $("#parameters1").toggle();
+  });
+
+  $(".returns").click(function(){
+    $("p").hide();
+    $("#returns1").toggle();
+  });
+
+  $(".chaining").click(function(){
+    $("p").hide();
+    $("#chaining1").toggle();
+  });
+
+  $(".strings").click(function(){
+    $("p").hide();
+    $("#strings1").toggle();
+  });
+
+  $(".booleans").click(function(){
+    $("p").hide();
+    $("#booleans1").toggle();
+  });
+
+  $(".undefined").click(function(){
+    $("p").hide();
+    $("#undefined1").toggle();
+  });
+
+  $(".NaN").click(function(){
+    $("p").hide();
+    $("#NaN").toggle();
+  });
+
+  $(".escape").click(function(){
+    $("p").hide();
+    $("#escape1").toggle();
+  });
+
+  $(".alerts").click(function(){
+    $("p").hide();
+    $("#alerts1").toggle();
+  });
+
+  $(".comments").click(function(){
+    $("p").hide();
+    $("#comments1").toggle();
+  });
+
+  $(".jQuery").click(function(){
+    $("p").hide();
+    $("#jQuery1").toggle();
+  });
+
+  $(".attributes").click(function(){
+    $("p").hide();
+    $("#attributes1").toggle();
+  });
+
+
 });
